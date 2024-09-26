1d0b1155be5a58f8a961729ed3d6a5b60b363ff4
tomipodo <podolskytomi@gmail.com>
2024-09-26 11:07:50 -0300

    .

--- src/main/java/ar/edu/utn/frba/dds/controladores/ControladorRegistro.java
+++ src/main/java/ar/edu/utn/frba/dds/controladores/ControladorRegistro.java
@@ -11,15 +11,20 @@ public class ControladorRegistro {
 
     public void guardarCampos(Context context) {
         System.out.println("Llegó esto: ");
-
+        //PREGUNTAR:
+        // SI EL NOMBRE ESTÁ EN NULL ==> ES JURÍDICA
+        // SI NO                     ==> ES HUMANA
         System.out.println(context.formParam("nombre"));
         System.out.println(context.formParam("apellido"));
         System.out.println(context.formParam("mail"));
         System.out.println(context.formParam("usuario"));
+        System.out.println(context.formParam("direccion"));
+        System.out.println(context.formParam("whatsapp"));
         System.out.println(context.formParam("donar-viandas"));
         System.out.println(context.formParam("donar-dinero"));
         System.out.println(context.formParam("distribuir-viandas"));
         System.out.println(context.formParam("administrar-heladeras"));
-        context.redirect("/");
+        //TODO: el 3 está hardcodeado, tiene q dirigir a el id posta -->
+        context.redirect("/3/home");
     }
 }
